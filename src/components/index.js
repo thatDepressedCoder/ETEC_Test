24598db50e1249b5dd50f1c51c0d79cde61c522b
Kenny Huang <kennyhuang@dyn-169-226-33-57.wireless.albany.edu>
2024-04-11 17:46:34 -0400

    Debug

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,11 @@
-import {
-    contact,
-    github,
-    linkedin,
-} from "../assets/icons";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import HomeInfo from "./HomeInfo";
 
-export const socialLinks = [
-    {
-        name: 'Email',
-        iconUrl: contact,
-        link: 'mailto:hakan.otal@outlook.com',
-    },
-    {
-        name: 'GitHub',
-        iconUrl: github,
-        link: 'https://github.com/hakanotal',
-    },
-    {
-        name: 'LinkedIn',
-        iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/hakan-tugrul-otal',
-    }
-];
+export {
+    Loader,
+    Navbar,
+    Footer,
+    HomeInfo
+}
