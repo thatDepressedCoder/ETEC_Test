6e12711ffbbfbfa797e2a60c2ef540fb4f2cdf76
betultok <tokhbetul@gmail.com>
2024-04-13 21:52:32 -0400

    changes6 - bt

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,7 +2,7 @@ const HomeInfo = (props) => {
   
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-yellow mx-5'>
-        Hello, Welcome to UAlbany's
+        Welcome to UAlbany's
         <br />
 
         <span className='font-semibold mx-2 text-gold'>ETEC</span>
