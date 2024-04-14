672b19285c89d3c16292ed699ade6d40f1e60674
betultok <tokhbetul@gmail.com>
2024-04-13 21:43:30 -0400

    cahnges4 - bt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className='header'>
-      <NavLink to='/'>
-        <h1 className='text-xl font-bold yellow-gradient_text drop-shadow'>
+      <NavLink to='/' className="navbar-link">
+        <h1 className='text-xl font-bold yellow-gradient_text drop-shadow navbar-text'>
         Emerging Technology and Entrepreneurship Complex
         </h1>
       </NavLink>
