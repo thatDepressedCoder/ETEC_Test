b40ab0be5a2ccc91f4bfb67d2a62e894a1135c4a
Kenny <Kennyhuang9314@gmail.com>
2024-04-13 21:48:31 -0400

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,7 @@ import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
-  return (
-    <header className='header'>
-      <NavLink to='/' className="navbar-link">
-        <h1 className='text-xl font-bold yellow-gradient_text drop-shadow navbar-text'>
-        Emerging Technology and Entrepreneurship Complex
-        </h1>
-      </NavLink>
-    </header>
-  );
+  
 };
 
 export default Navbar;
